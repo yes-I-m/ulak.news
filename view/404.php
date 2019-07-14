c1a29990593307bf6a2df46a43b1788d99cce8df
Orhan AYDOĞDU <info@orhanaydogdu.com.tr>
2019-07-15 02:45:44 +0300

    Update 404.php

--- view/404.php
+++ view/404.php
@@ -3,7 +3,7 @@
 						<div class="error-page-content">
 							<div class="not-found">
 								<h1>404</h1>
-								<p>Şu anda istediğiniz sayfaya ulaşamadık yada bir şeyler ters gitti.<br/> AMA <br/> Merak etmeyin herşey kontrol altında bu hatayı not aldık. :)</p>
+								<p>Şu anda istediğiniz sayfaya/habere ulaşamadık yada bir şeyler ters gitti.<br/> AMA <br/> Merak etmeyin herşey kontrol altında bu hatayı not aldık. :)</p>
 								<a style="margin-top: 10px;" href="index.html" class="btn btn-primary">Ana Sayfaya Dön</a>
                             </div>
                             <div class="logo text-center">
