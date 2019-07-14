5f3f206c0a45a584209151becc542e8d441bee9a
Orhan AYDOĞDU <info@orhanaydogdu.com.tr>
2019-07-15 02:35:36 +0300

    v1.1
    cache etkinleştirildi.

--- arama.php
+++ arama.php
@@ -1,4 +1,16 @@
 <?php
+	///// CACHE //////////
+		require_once "sCache.php";
+		$options = array(
+			'time'   => 60, // 60 saniye
+			'dir'    => 'cache/', // sCache2 klasörü oluşturup buraya yazılsın.
+			'load'   => false,  // sayfamızın sonunda load değerimiz görünsün.
+			'extension' => ".html", // standart değer .html olarak ayarlanmıştır cache dosyalarınızın uzantısını temsil etmektedir.
+			);
+		
+		$sCache = new sCache($options); // ayarları sınıfımıza gönderip sınıfı çalıştıralım.
+	///// CACHE BITIS /////
+	
 	include("funcs.php");
 	$desc="";
 	$q="";
--- kategori.php
+++ kategori.php
@@ -1,10 +1,22 @@
 <?php
+	///// CACHE //////////
+		require_once "sCache.php";
+		$options = array(
+			'time'   => 60, // 60 saniye
+			'dir'    => 'cache/', // sCache2 klasörü oluşturup buraya yazılsın.
+			'load'   => false,  // sayfamızın sonunda load değerimiz görünsün.
+			'extension' => ".html", // standart değer .html olarak ayarlanmıştır cache dosyalarınızın uzantısını temsil etmektedir.
+			);
+		
+		$sCache = new sCache($options); // ayarları sınıfımıza gönderip sınıfı çalıştıralım.
+	///// CACHE BITIS /////
+
 	include("funcs.php");
 	$desc="";
 	$q="";
 	$all_news=[];
 	$cat_status=false;
-	/// CACHE
+
 	if(isset($_GET['kategori'])){
 		$kategori=base64_decode($_GET['kategori']);
 		$all_news=get_cat_news($kategori);
