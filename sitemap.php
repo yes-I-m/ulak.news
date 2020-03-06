8f92590d59edc56d7e987f0e3176a9ddcb7cf871
Orhan AYDOĞDU <info@orhanaydogdu.com.tr>
2020-03-06 23:48:54 +0300

    v3.0.2
    fix

--- sitemap.php
+++ sitemap.php
@@ -1,23 +1,20 @@
 <?php
 
 header ("Content-Type:text/xml");
-
-include("funcs.php");
-
-if(!$is_local){
-    ///// CACHE //////////
-        require_once "sCache.php";
-        $options = array(
-            'time'   => 60*60*24, // 60 saniye
-            'dir'    => 'cache/', // sCache2 klasörü oluşturup buraya yazılsın.
-            'load'   => false,  // sayfamızın sonunda load değerimiz görünsün.
-            'extension' => ".html", // standart değer .html olarak ayarlanmıştır cache dosyalarınızın uzantısını temsil etmektedir.
-            );
-        
-        $sCache = new sCache($options); // ayarları sınıfımıza gönderip sınıfı çalıştıralım.
-    ///// CACHE BITIS /////
-}
 if(isset($_GET['page'])){
+
+    ///// CACHE //////////
+    require_once "sCache.php";
+    $options = array(
+        'time'   => 60*60*24, // 60 saniye
+        'dir'    => 'cache/', // sCache2 klasörü oluşturup buraya yazılsın.
+        'load'   => false,  // sayfamızın sonunda load değerimiz görünsün.
+        'extension' => ".xml", // standart değer .html olarak ayarlanmıştır cache dosyalarınızın uzantısını temsil etmektedir.
+        );
+    
+    $sCache = new sCache($options); // ayarları sınıfımıza gönderip sınıfı çalıştıralım.
+///// CACHE BITIS /////
+
     $page = preg_replace('/\D/', '', $_GET['page']);
     $data = file_get_contents("https://nodejs-api.ulak.news/sitemap_$page.xml");
     echo $data;
