e1c4d60456b0564ad423b53995312f84060cbb08
yes-I-m <73722886+yes-I-m@users.noreply.github.com>
2020-11-05 02:07:21 +0300

    Update atom_news.php

--- atom_news.php
+++ atom_news.php
@@ -25,7 +25,7 @@ if(isset($_GET['cat'])){
             $cat = "Güncel";
             break;
         case 'sondakika':
-            $cat = "sondakika";
+            $cat = "Son Dakika";
             break;
         default:
             $cat = null;
